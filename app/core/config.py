2e160d6ce3ee581dc96fbd33f05d0245d44e0c96
Methminpulsara <methminpulsara10@gmail.com>
2025-10-22 23:03:24 +0530

    update

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "taskflow")
+
+DATABASE_URL = f"mysql+mysqlclient://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
