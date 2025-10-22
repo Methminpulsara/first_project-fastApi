954eacbff93d29b756477b430525e0aac3236606
Methminpulsara <methminpulsara10@gmail.com>
2025-10-22 23:53:00 +0530

    create schema

---
+++ app/schemas/task_schema.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+
+class TaskCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class TaskOut(BaseModel):
+    id: int
+    title: str
+    description: Optional[str]
+    completed = bool
+    createdAt = datetime
+
+    class Config:
+        orm_mode = True
